6c62c5eb377f34c133509b9c6149e56f07043889
Jobayer Ahmed Mickey <jobayeramickey@gmail.com>
2017-11-13 15:33:01 +0600

    First Push

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express'),
+      app = express(),
+      bodyParser = require('body-parser'),
+      translate = require('google-translate-api');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/', (req, res)=>{res.render('index')});
+app.post('/translated', (req, res)=>{
+  let text = req.body.text;
+  translate(text, {to: 'bn'}).then(response => {
+    console.log(response.text)
+   // res.send(response.text)
+   res.render('translated', {text: response.text})
+  }).catch(err => {
+    console.error(err);
+  });
+})
+app.listen(3000,()=>{console.log('Server Connected')})
+
